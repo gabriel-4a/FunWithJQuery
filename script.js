58755a8f1b2d06e02dd38f3631a1eb27f029336d
student <student@admins-MacBook-Pro.local>
2019-03-02 12:11:32 -0800

    updated

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
 // all jQuery methods go here...
+$("#runningman-img").hover(enter, leave);
+function enter(){
+  this.src = "images/runningman-giphy.gif"
+}
+
+function leave(){
+this.src = "images/runningman-icon.png"
+}
+
+$("#taketheL-img").hover(enter1, leave1);
+function enter1(){
+  this.src = "images/taketheL-giphy.gif"
+}
+
+function leave1(){
+this.src = "images/taketheL-icon.png"
 
 
+}
 
+$("electroshuffle-img").hover(enter, leave);
+function enter(){
+  this.src="images/electroshuffle-giphy.gif"
+}
 
-
-
-
-
-
-
+function leave(){
+  this.src= "images/electroshuffle-icon.png"
+}
 
 
 
